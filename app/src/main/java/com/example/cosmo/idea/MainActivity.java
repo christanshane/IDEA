e48674f97781449deef11901040451f8ae40333c
cosmoTechnologist <cosmoTechnologist@yahoo.com>
2017-11-10 04:17:59 +0800

    Added Firebase

--- app/src/main/java/com/example/cosmo/idea/MainActivity.java
+++ app/src/main/java/com/example/cosmo/idea/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.cosmo.idea;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mFirebaseBtn = (Button)findViewById(R.id.firebaseButton);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDatabase.child("Name").setValue("Christan Shane D. Plaza");
+            }
+        });
     }
 }
