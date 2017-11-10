83610bd9f0db471191dd1052f8ca1fee53da71b3
cosmoTechnologist <cosmoTechnologist@yahoo.com>
2017-11-11 03:15:58 +0800

    updated basic node CRUD

--- app/src/main/java/com/example/cosmo/idea/MainActivity.java
+++ app/src/main/java/com/example/cosmo/idea/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,18 +12,25 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
+    private EditText nodeName, nodeValue;
+    private Button mFirebaseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button mFirebaseBtn = (Button)findViewById(R.id.firebaseButton);
+
+        nodeName = (EditText)findViewById(R.id.nodeNameTxt);
+        nodeValue = (EditText)findViewById(R.id.nodeValueTxt);
+        mFirebaseBtn = (Button)findViewById(R.id.firebaseBtn);
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mDatabase.child("User").setValue("Christan");
+                String name = nodeName.getText().toString();
+                String value = nodeValue.getText().toString();
+                mDatabase.child(name).setValue(value);
             }
         });
     }
