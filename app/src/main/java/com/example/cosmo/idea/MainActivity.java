1ff2ccd3484cf0da2da74e6de0b76528a469b012
cosmoTechnologist <cosmoTechnologist@yahoo.com>
2017-11-14 00:30:17 +0800

    updated firebase, now generates random key

--- app/src/main/java/com/example/cosmo/idea/MainActivity.java
+++ app/src/main/java/com/example/cosmo/idea/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.cosmo.idea;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
-    private EditText nodeName, nodeValue;
+    private EditText nodeName, nodeEmail;
     private Button mFirebaseBtn;
 
     @Override
@@ -21,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nodeName = (EditText)findViewById(R.id.nodeNameTxt);
-        nodeValue = (EditText)findViewById(R.id.nodeValueTxt);
+        nodeEmail = (EditText)findViewById(R.id.nodeEmailTxt);
         mFirebaseBtn = (Button)findViewById(R.id.firebaseBtn);
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -29,8 +35,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = nodeName.getText().toString();
-                String value = nodeValue.getText().toString();
-                mDatabase.child(name).setValue(value);
+                String email = nodeEmail.getText().toString();
+
+                HashMap<String, String> data = new HashMap<String, String>();
+                data.put("Name", name);
+                data.put("Email", email);
+
+                mDatabase.push().setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Data is stored!", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
     }
